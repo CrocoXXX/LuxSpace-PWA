d9e112053e4b243e12442823a1cacaf0d45b2ebe
CrocoXXX <lawrencenoman@gmail.com>
2023-10-17 23:37:31 +0700

    Cache JSON and Image from REST API -- DONE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Browse from './components/Browse';
@@ -8,12 +10,31 @@ import AsideMenu from './components/AsideMenu';
 import Footer from './components/Footer';
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    (async function () {
+      // Call data from API
+      const response = await fetch(' https://bwacharity.fly.dev/items', {
+        headers: {
+          'Content-Type': 'application/json',
+          'accept': 'application/json',
+          // 'x-api-key': process.env.REACT_APP_APIKEY
+        }
+      })
+
+      const { nodes } = await response.json()
+      // console.log(nodes);
+      setItems(nodes)
+    })()
+  }, [])
+
   return (
     <>
       <Header />
       <HeroSection />
       <Browse />
-      <Arrived />
+      <Arrived items={items} />
       <Clients />
       <AsideMenu />
       <Footer />
